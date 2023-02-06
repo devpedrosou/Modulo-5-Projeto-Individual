17c87ee00d9ba080eb02217d917200d778fef1db
Pedro Souza <113391888+themonsteer@users.noreply.github.com>
2023-02-06 19:00:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const propriedades = [];
 let inserir = "";
 
 while (inserir != "sair") {
-  propriedades.push(inserir);
   inserir = readlineSync
     .question("insira uma propriedade CSS: ")
     .toLocaleLowerCase();
+    if(inserir){
+      propriedades.push(inserir);
+    }
 }
-console.log((propriedades.sort().join("\n")));
+console.log((propriedades.sort().filter(Boolean).join("\n")));
